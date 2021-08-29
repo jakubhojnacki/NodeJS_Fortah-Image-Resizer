3f2dd83342a0c76583abd9c016aca14a5360b178
Jakub Hojnacki <jakubhojnacki@gmail.com>
2021-08-29 15:32:56 +0100

    Some small amendments

--- src/args/argTemplateFactory.js
+++ src/args/argTemplateFactory.js
@@ -13,8 +13,8 @@ import DataType from "../general/dataType.js";
 export default class ArgTemplateFactory {
     static get argTemplates() { 
         return new ArgTemplates([
-            new ArgTemplate(0, ArgName.source, "Source file pattern", DataType.string, true),
-            new ArgTemplate(1, ArgName.destination, "Path to destination directory", DataType.string, true),
+            new ArgTemplate(0, ArgName.source, "Source folder, file or file pattern", DataType.string, true),
+            new ArgTemplate(1, ArgName.destination, "Destination directory", DataType.string, true),
             new ArgTemplate(2, ArgName.sizes, "Comma-separated list of sizes", DataType.string, true),
             new ArgTemplate([ "ddp", "destinationDirectoryPattern" ], ArgName.directoryNameTemplate, "Pattern of destination directory", DataType.string),
             new ArgTemplate([ "dfp", "destinationFilePattern" ], ArgName.fileNameTemplate, "Pattern of destination file", DataType.string),
